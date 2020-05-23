8a28830102da11d5ca4057fdeffc55daca5cfc41
Wojtek Kruk <krukon@gmail.com>
2020-05-23 10:23:51 +0800

    Inheritance of interface annotations

--- jaxrs-processor/src/main/java/io/micronaut/jaxrs/processor/JaxRsTypeElementVisitor.java
+++ jaxrs-processor/src/main/java/io/micronaut/jaxrs/processor/JaxRsTypeElementVisitor.java
@@ -3,6 +3,7 @@ package io.micronaut.jaxrs.processor;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.HttpMethodMapping;
 import io.micronaut.http.annotation.UriMapping;
+import io.micronaut.inject.annotation.AnnotationMetadataHierarchy;
 import io.micronaut.inject.ast.ClassElement;
 import io.micronaut.inject.ast.MethodElement;
 import io.micronaut.inject.ast.ParameterElement;
@@ -56,7 +57,7 @@ public class JaxRsTypeElementVisitor implements TypeElementVisitor<Object, Objec
 
         }
 
-        if (element.hasDeclaredAnnotation(Path.class)) {
+        if (element.hasDeclaredAnnotation(Path.class) || hasAnnotationOnDeclaredMetadata(element, Path.class)) {
             element.annotate(HttpMethodMapping.class, builder ->
                     builder.value(element.stringValue(Path.class).orElse(UriMapping.DEFAULT_URI))
             );
@@ -70,5 +71,12 @@ public class JaxRsTypeElementVisitor implements TypeElementVisitor<Object, Objec
 
     private List<Class<? extends Annotation>> getUnsupportedParameterAnnotations() {
         return Arrays.asList(MatrixParam.class, BeanParam.class);
+    }
+
+    private boolean hasAnnotationOnDeclaredMetadata(MethodElement element, Class<? extends Annotation> annotation) {
+        return (element.getAnnotationMetadata() instanceof AnnotationMetadataHierarchy &&
+                ((AnnotationMetadataHierarchy) element.getAnnotationMetadata())
+                        .getDeclaredMetadata()
+                        .hasAnnotation(annotation));
     }
 }
