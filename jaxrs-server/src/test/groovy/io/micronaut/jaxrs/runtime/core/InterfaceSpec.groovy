a80667aab79bd82b1590b48581afdd9f0d25914d
Anze Sodja <anze.sodja@outfit7.com>
2021-01-09 19:08:17 +0100

    Remove client test

--- jaxrs-server/src/test/groovy/io/micronaut/jaxrs/runtime/core/InterfaceSpec.groovy
+++ jaxrs-server/src/test/groovy/io/micronaut/jaxrs/runtime/core/InterfaceSpec.groovy
@@ -4,7 +4,7 @@ package io.micronaut.jaxrs.runtime.core
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.client.annotation.Client
-import io.micronaut.jaxrs.runtime.InterfaceResourceClient
+
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
 
@@ -17,21 +17,9 @@ class InterfaceSpec extends Specification {
     @Client("/")
     RxHttpClient rootClient
 
-    @Inject
-    InterfaceResourceClient interfaceClient
-
     void 'test JAX-RS controller works with interface'() {
         when:
         def response = rootClient.toBlocking().exchange("/api/interface/test/ping/hello", String.class)
-
-        then:
-        response.status() == HttpStatus.OK
-        response.body() == "hello"
-    }
-
-    void 'test JAX-RS client works with interface'() {
-        when:
-        def response = interfaceClient.ping("hello")
 
         then:
         response.status() == HttpStatus.OK
