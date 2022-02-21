3d0562f270c00bd01a653511c321f98abcc87034
Guillermo Calvo <guillermo@guillermo.dev>
2022-02-21 18:23:20 +0100

    fix: support for JAX RS exceptions #164

---
+++ jaxrs-server/src/main/java/io/micronaut/jaxrs/runtime/core/JaxRsExceptionHandler.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.jaxrs.runtime.core;
+
+import io.micronaut.context.annotation.Primary;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.server.exceptions.ExceptionHandler;
+import jakarta.inject.Singleton;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+/**
+ * Handles JAX-RS exceptions that occur during the execution of an HTTP request.
+ *
+ * @author guillermocalvo
+ */
+@Singleton
+@Primary
+public class JaxRsExceptionHandler
+    implements ExceptionHandler<WebApplicationException, HttpResponse<?>> {
+
+    @Override
+    public HttpResponse<?> handle(HttpRequest request, WebApplicationException exception) {
+        final Response body = exception.getResponse();
+        if (body instanceof JaxRsResponse) {
+            return ((JaxRsResponse) body).getResponse();
+        }
+        throw exception;
+    }
+}
