6de66e02f517e0649e0c00ec921c9d0671dafcfa
Dean Wette <wetted@objectcomputing.com>
2023-03-09 04:50:45 -0600

    build: use the new validation module (#281)
    * Change build to use new validation module (i.e. `mnValidation.micronaut.validation`, etc)
    * fix broken graalvm.yml
    * build to 6.3.3, gradle to 8.0.2

--- jaxrs-server/build.gradle.kts
+++ jaxrs-server/build.gradle.kts
@@ -13,14 +13,14 @@ dependencies {
 
 	// for Java
 	testAnnotationProcessor(mn.micronaut.inject.java)
-	testAnnotationProcessor(mn.micronaut.validation)
-	testAnnotationProcessor(projects.jaxrsProcessor)
+	testAnnotationProcessor(mnValidation.micronaut.validation.processor)
+	testAnnotationProcessor(projects.micronautJaxrsProcessor)
 
-	testImplementation(projects.jaxrsProcessor)
+	testImplementation(projects.micronautJaxrsProcessor)
     testImplementation(mnSerde.micronaut.serde.jackson)
 	testImplementation(mn.micronaut.http.server.netty)
 	testImplementation(mn.micronaut.http.client)
-	testImplementation(mn.micronaut.validation)
+	testImplementation(mnValidation.micronaut.validation)
 	testImplementation(mnTest.micronaut.test.junit5)
 	testRuntimeOnly(libs.junit.jupiter.engine)
 }
