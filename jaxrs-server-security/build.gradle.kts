74d962f636b6d6bbd35d486dfbb58449fef8bd70
Tim Yates <tim.yates@gmail.com>
2024-02-28 15:31:29 +0000

    Spaces not tabs in build files

--- jaxrs-server-security/build.gradle.kts
+++ jaxrs-server-security/build.gradle.kts
@@ -3,23 +3,23 @@ plugins {
 }
 
 dependencies {
-	annotationProcessor(mn.micronaut.graal)
+    annotationProcessor(mn.micronaut.graal)
 
-	implementation(projects.micronautJaxrsServer)
+    implementation(projects.micronautJaxrsServer)
     implementation(mnSecurity.micronaut.security)
 
-	testAnnotationProcessor(mnValidation.micronaut.validation.processor)
-	testAnnotationProcessor(mn.micronaut.inject.java)
-	testAnnotationProcessor(projects.micronautJaxrsProcessor)
-	testAnnotationProcessor(mnSecurity.micronaut.security.annotations)
+    testAnnotationProcessor(mnValidation.micronaut.validation.processor)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+    testAnnotationProcessor(projects.micronautJaxrsProcessor)
+    testAnnotationProcessor(mnSecurity.micronaut.security.annotations)
     testAnnotationProcessor(mnSerde.micronaut.serde.processor)
 
     testImplementation(mnValidation.micronaut.validation)
-	testImplementation(libs.managed.jaxrs.api)
-	testImplementation(mn.micronaut.http.client)
-	testImplementation(mn.micronaut.http.server.netty)
-	testImplementation(mnTest.micronaut.test.junit5)
+    testImplementation(libs.managed.jaxrs.api)
+    testImplementation(mn.micronaut.http.client)
+    testImplementation(mn.micronaut.http.server.netty)
+    testImplementation(mnTest.micronaut.test.junit5)
     testImplementation(mnSerde.micronaut.serde.jackson)
 
-	testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
