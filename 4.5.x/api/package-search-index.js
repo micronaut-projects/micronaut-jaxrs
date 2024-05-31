e9a3e9fcc7ffab13a48753870d6430df17003615
graemerocher <graemerocher@users.noreply.github.com>
2024-05-31 14:37:18 +0000

    Deploying to gh-pages - 14:37:17

---
+++ 4.5.x/api/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.jaxrs.processor"},{"l":"io.micronaut.jaxrs.runtime.core"},{"l":"io.micronaut.jaxrs.runtime.ext.bind"},{"l":"io.micronaut.jaxrs.runtime.ext.convert"},{"l":"io.micronaut.jaxrs.runtime.ext.impl"},{"l":"io.micronaut.jaxrs.security.bind"}];updateSearchResults();
