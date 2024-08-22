b22faf376d44aa4400f48537d076b32ec624525a
Denis Stepanov <denis.s.stepanov@oracle.com>
2024-08-22 16:12:46 +0200

    Update jaxrs-client/build.gradle.kts
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- jaxrs-client/build.gradle.kts
+++ jaxrs-client/build.gradle.kts
@@ -18,19 +18,4 @@ dependencies {
 	implementation(mn.micronaut.http.client)
     implementation(mn.micronaut.buffer.netty)
 
-	// for Java
-	testAnnotationProcessor(mn.micronaut.inject.java)
-	testAnnotationProcessor(mnValidation.micronaut.validation.processor)
-	testAnnotationProcessor(projects.micronautJaxrsProcessor)
-
-	testImplementation(projects.micronautJaxrsProcessor)
-    testImplementation(mnSerde.micronaut.serde.jackson)
-	testImplementation(mn.micronaut.http.server.netty)
-	testImplementation(mn.micronaut.http.client)
-	testImplementation(mnValidation.micronaut.validation)
-	testImplementation(mnTest.micronaut.test.junit5)
-    testImplementation(libs.junit.jupiter.params)
-
-    testRuntimeOnly(mnLogging.logback.classic)
-	testRuntimeOnly(libs.junit.jupiter.engine)
 }
