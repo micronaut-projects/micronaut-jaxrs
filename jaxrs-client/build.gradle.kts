41e7cf1e282af2068591e836cc8e0a597913b88a
Sergio del Amo <sergio.delamo@softamo.com>
2024-08-23 11:23:17 +0200

    Update build.gradle.kts

--- jaxrs-client/build.gradle.kts
+++ jaxrs-client/build.gradle.kts
@@ -2,20 +2,11 @@ plugins {
     id("io.micronaut.build.internal.jaxrs-module")
 }
 
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}
-
 dependencies {
-
-	api(mn.micronaut.http)
-	api(libs.managed.jaxrs.api)
-	api(libs.managed.jaxrs.api)
-
-	implementation(projects.micronautJaxrsCommon)
-	implementation(mn.micronaut.http.client)
+    api(mn.micronaut.http)
+    api(libs.managed.jaxrs.api)
+    api(libs.managed.jaxrs.api)
+    implementation(projects.micronautJaxrsCommon)
+    implementation(mn.micronaut.http.client)
     implementation(mn.micronaut.buffer.netty)
-
 }
