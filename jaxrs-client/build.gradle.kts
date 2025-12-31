3ef9d901abce7cdad1716638a4bb36f530ecc007
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-31 09:01:20 +0100

    remove ossIndex exclude

--- jaxrs-client/build.gradle.kts
+++ jaxrs-client/build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
     id("io.micronaut.build.internal.jaxrs-module")
 }
-
 dependencies {
     api(mn.micronaut.http)
     api(libs.managed.jaxrs.api)
-
     implementation(projects.micronautJaxrsCommon)
     implementation(mn.micronaut.http.client)
     implementation(mn.micronaut.buffer.netty)
