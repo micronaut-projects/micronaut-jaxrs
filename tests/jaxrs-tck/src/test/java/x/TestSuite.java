a118a86d6ef71cd506bd1f0048ad6a99c9105762
Graeme Rocher <graeme.rocher@oracle.com>
2026-05-27 11:41:00 +0200

    Track request matching TCK failures in failingTests

--- tests/jaxrs-tck/src/test/java/x/TestSuite.java
+++ tests/jaxrs-tck/src/test/java/x/TestSuite.java
@@ -19,9 +19,6 @@ public class TestSuite {
     @SelectPackages("ee.jakarta.tck.ws.rs")
     @ExcludePackages({EE_TEST_PACKAGE, API_TEST_PACKAGE})
     @IncludeClassNamePatterns(".*")
-    @ExcludeClassNamePatterns({
-        "ee.jakarta.tck.ws.rs.spec.resource.requestmatching.JAXRSClientIT"
-    })
     public static class Tests {
     }
 
